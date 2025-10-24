5ad219ef54b720b617555112d912e84239bf92d7
Hossein Rahmani <rahmani.hosseine@gmail.com>
2025-10-24 22:38:19 +0330

    Delete tailwind.config.cjs

--- tailwind.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: { colors: { gold: '#c9a34a', darkbg: '#0b0b0b', graymuted: '#bfb7a6' } } },
-  plugins: []
-}
