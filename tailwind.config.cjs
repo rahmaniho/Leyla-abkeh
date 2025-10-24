65dc2bebc5763d8691675bc7d75f116da26e2e9e
rahmaniho <rahmani.hosseine@gmail.com>
2025-10-24 16:05:49 +0330

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: { extend: { colors: { gold: '#c9a34a', darkbg: '#0b0b0b', graymuted: '#bfb7a6' } } },
+  plugins: []
+}
